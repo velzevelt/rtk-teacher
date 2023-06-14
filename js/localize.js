c5cfdfbf196d697f3c84ea597908b01ceb4040ae
velzevelt <novaraptor13@gmail.com>
2023-06-15 00:35:13 +0300

    add bg

--- js/localize.js
+++ js/localize.js
@@ -2,6 +2,8 @@
 
 $(function () {
     var lang = navigator.language || navigator.userLanguage; //no ?s necessary
+    // lang = 'ru'
+
     var opts = { language: lang, pathPrefix: "./localization/" };
     $("[data-localize]").localize("localization", opts)
 })
